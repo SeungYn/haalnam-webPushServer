304ace0bef21c4154f490a3bb362c4a6f95188ae
SeungYn <kwls0407@gmail.com>
2024-09-05 23:46:43 +0900

    feat: 에러 처리 추가 및 404 라우터 추가

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { initWebPushSchedules } from './service/planWebPushService';
 import planWebPushRouter from './router/planWebPushRouter';
+import { initExpiredSubscriptionProcessSchedule } from './util/webPush';
 
 dotenv.config();
 
@@ -11,7 +12,18 @@ app.use(express.json());
 
 app.use('/schedule', planWebPushRouter);
 
-app.listen(4000, () => {
+app.use((req, res, next) => {
+  res.status(404).json({ message: '라우터가 존재하지 않습니다.' });
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.log('----에러 발생---', new Date());
+  console.error(err);
+  res.status(500).json({ message: '에러가 발생했습니다.' });
+});
+
+const server = app.listen(4000, async () => {
   console.log('express start');
-  initWebPushSchedules();
+  await initWebPushSchedules();
+  initExpiredSubscriptionProcessSchedule();
 });
