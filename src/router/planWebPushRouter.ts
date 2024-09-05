be07883a1fa35abc0ea56bf319cbe9107ec329d9
SeungYn <kwls0407@gmail.com>
2024-09-05 23:46:02 +0900

    feat: 라우터에서 데이터 으압받고 반환하도록 추가

--- src/router/planWebPushRouter.ts
+++ src/router/planWebPushRouter.ts
@@ -1,10 +1,21 @@
 import express from 'express';
 import * as webPushService from '../service/planWebPushService';
+import { WebPushDTO } from '../types/WebPushDTO';
 
 const router = express.Router();
 
-router.post('/', webPushService.addWebPushSchedule);
+router.post('/', (req, res, next) => {
+  const users: WebPushDTO.User[] = req.body;
+  webPushService.updateWebPushSchedule(users);
 
-router.delete('/', webPushService.deleteWebPushSchedule);
+  return res.status(204).end();
+});
+
+router.delete('/', (req, res, next) => {
+  const user: Pick<WebPushDTO.User, 'id'> = req.body;
+  webPushService.deleteWebPushSchedule(user);
+
+  return res.status(204).end();
+});
 
 export default router;
